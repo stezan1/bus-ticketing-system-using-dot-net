7b4d98fdcc83f88636d467004ebcfaff40ea7a3c
stezan1 <83536789+stezan1@users.noreply.github.com>
2023-05-09 13:58:31 +0545

    Add files via upload

---
+++ dbo.employeetry.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[employeetry] (
+    [employee_id]          INT           IDENTITY (1, 1) NOT NULL,
+    [employee_name]        NVARCHAR (50) NOT NULL,
+    [employee_addr]        NVARCHAR (50) NOT NULL,
+    [employee_contact]     NVARCHAR (50) NOT NULL,
+    [email]                NVARCHAR (50) NOT NULL,
+    [citizenhsip_no]       NVARCHAR (50) NOT NULL,
+    [father_name]          NVARCHAR (50) NOT NULL,
+    [age_sex]              NVARCHAR (50) NOT NULL,
+    [realationship_status] NVARCHAR (50) NOT NULL,
+    [username]             NVARCHAR (50) NOT NULL,
+    [password]             NVARCHAR (50) NOT NULL,
+    [type]                 NVARCHAR (50) NOT NULL,
+    [dob]                  DATETIME      NULL
+);
+
---
+++ dbo.tickettry.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[tickettry] (
+    [ticket_id]     INT           IDENTITY (1, 1) NOT NULL,
+    [ticket_no]     NVARCHAR (50) NOT NULL,
+    [p_name]        NVARCHAR (50) NOT NULL,
+    [p_addr]        NVARCHAR (50) NOT NULL,
+    [p_contact]     BIGINT        NOT NULL,
+    [age_sex]       NVARCHAR (50) NOT NULL,
+    [no_of_tickets] INT           NOT NULL,
+    [t_from]        NVARCHAR (50) NOT NULL,
+    [t_to]          NVARCHAR (50) NOT NULL,
+    [bus_no]        NVARCHAR (50) NOT NULL,
+    [bus_type]      NVARCHAR (50) NOT NULL,
+    [price]         FLOAT (53)    NOT NULL,
+    [seat]          NVARCHAR (50) NULL,
+    [date]          DATETIME      NULL,
+    [time]          NVARCHAR (50) NULL
+);
+
